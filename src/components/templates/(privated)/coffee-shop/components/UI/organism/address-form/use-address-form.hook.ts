b0a59e3742e667394c7957b83583fc093e2acd5d
leonardorossi <08.leo.rossi@gmail.com>
2024-10-07 19:54:31 -0300

    :bug: fix: Corrigindo rota errada do front-end

--- src/components/templates/(privated)/coffee-shop/components/UI/organism/address-form/use-address-form.hook.ts
+++ src/components/templates/(privated)/coffee-shop/components/UI/organism/address-form/use-address-form.hook.ts
@@ -3,9 +3,9 @@ import { useRouter } from 'next/navigation';
 export const useAddressFormHook = () => {
   const router = useRouter();
 
-  const handleBack = () => router.push('/coffee-shop/create/step-1');
+  const handleBack = () => router.push('/coffee-shops/create/step-1');
 
-  const handleContinue = () => router.push('/coffee-shop/create/step-3');
+  const handleContinue = () => router.push('/coffee-shops/create/step-3');
 
   return {
     handleBack,
