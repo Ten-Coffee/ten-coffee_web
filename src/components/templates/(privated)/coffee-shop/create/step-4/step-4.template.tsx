49ac7528045822e9222f80fe286dc6258517d401
samel <samuelrigo@gmail.com>
2024-10-07 22:26:30 -0300

    🐛fix: remoção do queryclientprovider

--- src/components/templates/(privated)/coffee-shop/create/step-4/step-4.template.tsx
+++ src/components/templates/(privated)/coffee-shop/create/step-4/step-4.template.tsx
@@ -5,20 +5,15 @@ import { DataRevisionSectionOrganism } from '@/components/templates/(privated)/c
 import { coffeeShopStepBoxData } from '@/components/templates/(privated)/coffee-shop/create/data/coffee-shop-step-box.data';
 import { TitleAtom } from '@/components/UI/atoms/typography/title/title.atom';
 import { StepBoxOrganism } from '@/components/UI/organism/step-box/step-box.organism';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-const queryClient = new QueryClient();
 
 export default function Step4Template() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <>
-        <TitleAtom.Large value={'Revisão de Dados'} />
-        <div className={'step-wrapper'}>
-          <StepBoxOrganism steps={coffeeShopStepBoxData} />
-          <DataRevisionSectionOrganism />
-        </div>
-      </>
-    </QueryClientProvider>
+    <>
+      <TitleAtom.Large value={'Revisão de Dados'} />
+      <div className={'step-wrapper'}>
+        <StepBoxOrganism steps={coffeeShopStepBoxData} />
+        <DataRevisionSectionOrganism />
+      </div>
+    </>
   );
 }
