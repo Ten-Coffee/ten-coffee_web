dfe083eccd1c5ade2c6a689c69eac4b8a28c3282
samel <samuelrigo@gmail.com>
2024-11-04 21:42:49 -0300

    ♻️refactor: tratamento de erros e post feito corretamente

--- src/components/templates/(privated)/coffee-shop/components/UI/organism/data-revision-section/use-data-revision-section-form.hook.ts
+++ src/components/templates/(privated)/coffee-shop/components/UI/organism/data-revision-section/use-data-revision-section-form.hook.ts
@@ -1,64 +1,60 @@
-'use client';
-
-import { labelMapping } from '@/components/templates/(privated)/coffee-shop/components/UI/organism/data-revision-section/utils/LabelMapping';
-import { DataItem } from '@/components/templates/(privated)/coffee-shop/components/UI/organism/data-revision/interfaces/data-item.interface';
 import { useFormStore } from '@/components/templates/(privated)/coffee-shop/create/store/coffee-shop-store';
 import { useMutation } from '@tanstack/react-query';
 import { useRouter } from 'next/navigation';
+import { toast } from 'react-toastify';
 
 export const useDataRevisionFormHook = () => {
   const router = useRouter();
-  const { formData } = useFormStore();
-
-  const { coffeeShop, address, representative } = formData;
+  const { formatData, formData, extractData } = useFormStore();
+  const { coffeeShop, representative, address } = formData;
+  const requestData = formatData();
 
   const mutation = useMutation({
     mutationFn: async () => {
-      const response = await fetch('https://seu-backend.com/api/cadastrar', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(formData)
-      });
-      if (!response.ok) {
-        throw new Error('Erro ao cadastrar');
+      try {
+        const response = await fetch('http://localhost:8080/coffeeShops', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(requestData)
+        });
+
+        if (!response.ok) {
+          const errorMessage = await response.text();
+          throw new Error(errorMessage);
+        }
+
+        const data = response.status !== 204 ? await response.json() : null;
+
+        toast.success('Cadastro realizado com sucesso!');
+        return data;
+      } catch (error) {
+        const errorMessage =
+          error instanceof Error ? error.message : 'Erro desconhecido';
+        toast.error(`${errorMessage}`);
+        throw error;
       }
-      return response.json();
-    },
-    onSuccess: () => {
-      router.push('/coffee-shop/');
-    },
-    onError: (error) => {
-      console.error('Erro ao cadastrar:', error);
     }
   });
 
-  const handleBack = () => {
-    router.back();
-  };
-
-  const handleCreate = () => {
-    mutation.mutate();
-  };
-
-  const extractData = (data: Record<string, unknown>): DataItem[] => {
-    return Object.entries(data)
-      .filter(([, value]) => value !== undefined && value !== '')
-      .map(([key, value]) => {
-        return {
-          label: labelMapping[key] || key,
-          value: String(value)
-        };
-      });
+  const handleCreate = async () => {
+    try {
+      await mutation.mutateAsync();
+      setTimeout(() => {
+        router.push('/coffee-shops');
+      }, 3500);
+    } catch (error) {
+      console.error('Erro ao criar coffee shop:', error);
+    }
   };
 
   return {
+    handleBack: () => router.back(),
+    handleCreate,
+    mutation,
     coffeeShop: extractData(coffeeShop),
     address: extractData(address),
-    representative: extractData(representative),
-    handleBack,
-    handleCreate,
-    mutation
+    representative: extractData(representative)
   };
 };
