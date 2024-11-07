332155e8ec0a315b0d184ba9baf36d9271e9daa3
samel <samuelrigo@gmail.com>
2024-11-06 21:58:10 -0300

    ♻️refactor: mudança na lógica de dar push (deveria ter sido feito com a mudança do toast)

--- src/components/templates/(privated)/coffee-shop/components/UI/organism/data-revision-section/use-data-revision-section-form.hook.ts
+++ src/components/templates/(privated)/coffee-shop/components/UI/organism/data-revision-section/use-data-revision-section-form.hook.ts
@@ -11,42 +11,36 @@ export const useDataRevisionFormHook = () => {
 
   const mutation = useMutation({
     mutationFn: async () => {
-      try {
-        const response = await fetch('http://localhost:8080/coffeeShops', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(requestData)
-        });
+      const response = await fetch('http://localhost:8080/coffeeShops', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(requestData)
+      });
 
-        if (!response.ok) {
-          const errorMessage = await response.text();
-          throw new Error(errorMessage);
-        }
-
-        const data = response.status !== 204 ? await response.json() : null;
-
-        toast.success('Cadastro realizado com sucesso!');
-        return data;
-      } catch (error) {
-        const errorMessage =
-          error instanceof Error ? error.message : 'Erro desconhecido';
-        toast.error(`${errorMessage}`);
-        throw error;
+      if (!response.ok) {
+        const errorMessage = await response.text();
+        throw new Error(errorMessage);
       }
+
+      const data = response.status !== 204 && (await response.json());
+
+      toast.success('Cadastro realizado com sucesso!');
+      return data;
+    },
+    onSuccess: () => {
+      router.push('/coffee-shops');
+    },
+    onError: (error) => {
+      const errorMessage =
+        error instanceof Error ? error.message : 'Erro desconhecido';
+      toast.error(`${errorMessage}`);
     }
   });
 
-  const handleCreate = async () => {
-    try {
-      await mutation.mutateAsync();
-      setTimeout(() => {
-        router.push('/coffee-shops');
-      }, 3500);
-    } catch (error) {
-      console.error('Erro ao criar coffee shop:', error);
-    }
+  const handleCreate = () => {
+    mutation.mutate();
   };
 
   return {
