6844a8b598ef2a50bcf6e12820174e4438f5eeee
samel <samuelrigo@gmail.com>
2024-11-11 20:14:41 -0300

    ♻️refactor: adição de queryclient e import

--- src/components/UI/atoms/wrapper/wrapper.atom.tsx
+++ src/components/UI/atoms/wrapper/wrapper.atom.tsx
@@ -3,8 +3,11 @@
 import { useWrapperHook } from '@/components/UI/atoms/wrapper/use-wrapper.hook';
 import { ToastMolecule } from '@/components/UI/molecules/toast/toast.molecule';
 import { SidebarOrganism } from '@/components/UI/organism/sidebar/sidebar.organism';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactNode } from 'react';
 import './wrapper.style.scss';
+
+const queryClient = new QueryClient();
 
 interface WrapperAtomProps {
   children: ReactNode;
