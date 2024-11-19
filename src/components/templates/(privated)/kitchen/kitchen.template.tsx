bd1718560eb7018d8a7dcd0ecc74212067a0cdac
LuigiCapoia <luigicapoia@gmail.com>
2024-11-18 23:42:06 -0300

    ♻️ refactor: deletando código ruim e obsoleto

--- src/components/templates/(privated)/kitchen/kitchen.template.tsx
+++ src/components/templates/(privated)/kitchen/kitchen.template.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import CardKitchen from '@/components/UI/molecules/card-kitchen/card-kitchen.molecule';
 import './kitchen-template.scss';
 
 export const KitchenTemplate = () => {
