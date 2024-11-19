9b4c9ff749a51dbf9aa0b7232fcd97fad2d2c1ab
LuigiCapoia <luigicapoia@gmail.com>
2024-11-18 23:40:38 -0300

    ♻️ refactor: deletando código ruim e obsoleto

--- src/components/templates/(privated)/kitchen/kitchen.template.tsx
+++ src/components/templates/(privated)/kitchen/kitchen.template.tsx
@@ -3,19 +3,5 @@ import CardKitchen from '@/components/UI/molecules/card-kitchen/card-kitchen.mol
 import './kitchen-template.scss';
 
 export const KitchenTemplate = () => {
-  return (
-    <div className="kitchen-template">
-      {Array.from({ length: 5 }).map((_, index) => (
-        <CardKitchen
-          key={index}
-          orderNumber={`00${index + 1}`}
-          tableNumber={`00${index + 1}`}
-          statusText="Preparo"
-          statusColor="#FFA000"
-          preparationTime={`${15 + index * 5} min`}
-          itemCount={`${2 + index} itens`}
-        />
-      ))}
-    </div>
-  );
+  return <div className="kitchen-template"></div>;
 };
