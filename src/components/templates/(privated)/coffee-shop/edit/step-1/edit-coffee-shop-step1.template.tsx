684865a8d5f544b81d7035c138a2c944dfadd8c6
leonardorossi <08.leo.rossi@gmail.com>
2024-11-24 19:10:11 -0300

    refactor: ajustes finos

--- src/components/templates/(privated)/coffee-shop/edit/step-1/edit-coffee-shop-step1.template.tsx
+++ src/components/templates/(privated)/coffee-shop/edit/step-1/edit-coffee-shop-step1.template.tsx
@@ -1,17 +1,20 @@
 'use client';
 
 import { OverviewOrganism } from '@/components/templates/(privated)/coffee-shop/components/UI/organism/overview/overview.organism';
+import { useEditCoffeeShopStep1Hook } from '@/components/templates/(privated)/coffee-shop/edit/step-1/use-edit-coffee-shop-step1.hook';
 import { editCoffeeShopSteps } from '@/components/templates/(privated)/coffee-shop/edit/steps/edit-coffee-shop-steps';
 import { TitleAtom } from '@/components/UI/atoms/typography/title/title.atom';
 import { StepBoxOrganism } from '@/components/UI/organism/step-box/step-box.organism';
 
 export const EditCoffeeShopStep1Template = () => {
+  const { address, coffeeShop } = useEditCoffeeShopStep1Hook();
+
   return (
     <>
       <TitleAtom.Large value={'Editar Credenciais'} />
       <div className={'step-wrapper'}>
         <StepBoxOrganism steps={editCoffeeShopSteps} />
-        <OverviewOrganism />
+        <OverviewOrganism address={address} coffeeShop={coffeeShop} />
       </div>
     </>
   );
